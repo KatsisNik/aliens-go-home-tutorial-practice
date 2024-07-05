5058ee6f944c29106445917d1c420c6db526df23
katsisnik <katsisnik@gmail.com>
2024-07-05 12:04:19 +0100

    Started working on the tutorial

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    message: `It's easy to integrate React and redux, isn't it?`,
+};
+
+function reducer(state = initialState) {
+    return state;
+}
+
+export default reducer;
